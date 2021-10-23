97eaaeebba0e6b793fdd7d7e3dd7aac412eefd49
Paul Schifferer <paul@schifferers.net>
2021-10-23 10:43:36 -0700

    Add src and test stubs

---
+++ src/sweetrpg_sdk/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+__author__ = "Paul Schifferer <dm@sweetrpg.com>"
+"""Metadata.
+"""
+
+__title__ = "sweetrpg-sdk"
+__description__ = "SweetRPG SDK"
+__url__ = "https://github.com/sweetrpg/sdk"
+__version__ = "0.0.0"
+__build__ = 0x000001
+__author__ = "Paul Schifferer"
+__author_email__ = "dm@sweetrpg.com"
+__license__ = "MIT"
+__copyright__ = "Copyright 2021 SweetRPG"
+__cake__ = "\u2728 \U0001f370 \u2728"
