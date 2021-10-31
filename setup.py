bdb12bae5aca929017838b48dc50dd760e5fc040
Paul Schifferer <paul@schifferers.net>
2021-10-30 20:39:11 -0700

    Update requirements

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
     name="sweetrpg-client",
-    install_requires=["marshmallow==3.14.0", "sweetrpg-model-core", "sweetrpg-api-core", "sweetrpg-library-objects", "requests"],
+    install_requires=["marshmallow==3.14.0",
+                      "sweetrpg-model-core",
+                      "sweetrpg-api-core",
+                      "sweetrpg-library-objects",
+                      "requests"],
     extras_require={},
 )
