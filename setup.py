6a8ca31748c3b01c99a79397eeb914b8f8ba6f12
Paul Schifferer <paul@schifferers.net>
2021-11-17 06:57:54 -0800

    Update versions.

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
     name="sweetrpg-client",
-    install_requires=["marshmallow==3.14.0",
+    install_requires=["marshmallow==3.14.1",
                       "sweetrpg-model-core",
                       "sweetrpg-api-core",
                       "sweetrpg-library-objects",
-                      "requests"],
+                      "requests", "jsonapi-client", "json-api-doc"],
     extras_require={},
 )
