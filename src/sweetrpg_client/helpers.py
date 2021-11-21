3960452d3f7b14dc05e63fd31ba78e7cf752e35f
Paul Schifferer <paul@schifferers.net>
2021-11-21 15:08:40 -0800

    Add logging

--- src/sweetrpg_client/helpers.py
+++ src/sweetrpg_client/helpers.py
@@ -13,12 +13,13 @@ def _flatten_object(obj):
     logging.debug("obj: %s", obj)
 
     fields = list(filter(lambda s: not s.startswith('_'), dir(obj.fields)))
+    logging.debug("fields: %s", fields)
     flattened = {'id': obj.id}
     for k in fields:
         try:
             flattened[k] = obj.attributes[k]
         except:
-            pass
+            logging.debug(f"value not found for key {k} in object {obj}")
     logging.debug("flattened: %s", flattened)
 
     return flattened
